98cf102732a6ff7b43b2e40ddaa7095f8cd31998
Laura <lmcsay@gmail.com>
2022-11-02 17:02:54 -0500

    Create ProfileIcon component with a dropdown

---
+++ src/components/Profile/ProfileIcon.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
+class ProfileIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dropdownOpen: false,
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  };
+
+  render() {
+    return (
+      <div className="pa4 tc">
+        <Dropdown
+          isOpen={this.state.dropdownOpen}
+          toggle={this.toggle}
+          drop="left"
+        >
+          <DropdownToggle
+            tag="span"
+            onClick={this.toggle}
+            data-toggle="dropdown"
+            aria-expanded={this.state.dropdownOpen}
+          >
+            <img
+              src="https://ps.w.org/simple-user-avatar/assets/icon-256x256.png?rev=2413146"
+              className="br-100 h3 w3 dib"
+              alt="avatar"
+            />
+          </DropdownToggle>
+          <DropdownMenu
+            className="b--transparent shadow-5"
+            style={{
+              marginLeft: '-6rem',
+              backgroundColor: 'rgba(255, 255, 255, 0.5)',
+            }}
+          >
+            <DropdownItem onClick={() => this.props.toggleModal()}>
+              View Profile
+            </DropdownItem>
+            <DropdownItem onClick={() => this.props.onRouteChange('signout')}>
+              Sign Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </div>
+    );
+  }
+}
+
+export default ProfileIcon;
