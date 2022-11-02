0cf591cf3dcf48f22a1a6ddcc4474cd23712e27e
Laura <lmcsay@gmail.com>
2022-11-02 17:01:20 -0500

    Import style packages and order correctly

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import 'tachyons';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />)
-
-// If using React version lower than 18:
-// ReactDOM.render(<App />, document.getElementById('root'));
-// registerServiceWorker();
+root.render(<App />);
